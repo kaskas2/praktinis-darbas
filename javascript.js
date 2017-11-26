f882fbe158ec385bc5f7a41df588a65e92170915
Kevin Smith <15814565+kaskas2@users.noreply.github.com>
2017-11-25 18:05:32 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,11 +3,6 @@ var x;
 var a = 3;
 var b = 071;
 
-//compare
-if (x === -0) {
-    // doSomething()
-}
-
 //reassign variables
 delete x;
 var a = 5;
@@ -15,14 +10,3 @@ var a = 5;
 if (b === a) {
     // doSomething()
 }
-
-//
-
-if (foo) foo++;
-
-while (bar)
-    baz();
-
-if (foo) {
-    baz();
-} else qux();
