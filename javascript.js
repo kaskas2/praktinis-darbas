cb66974a9605ac42eadb5c1e648cba093d64f4a1
Kevin Smith <15814565+kaskas2@users.noreply.github.com>
2017-11-25 18:32:14 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,17 +1,12 @@
 // assign variables
 var x;
 var a = 3;
-var b = "071";
+var b = 071;
 
 //reassign variables
-// delete x;
+delete x;
 var a = 5;
 
 if (b === a) {
-    // doSomething()
-}
-
-//compare
-if (x === -0) {
     // doSomething()
 }
